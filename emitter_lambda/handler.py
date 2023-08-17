4ffad93534388c2fa5e36f524f6e2cde21dfa028
alixpeigue <irobot.moi@gmail.com>
2023-08-17 22:01:24 +0200

    test emitter function access to internet (working)

--- emitter_lambda/handler.py
+++ emitter_lambda/handler.py
@@ -8,10 +8,19 @@ sqs_url = os.environ["sqs"]
 def lambda_handler(event, context):
     sqs = boto3.client('sqs')
     print("client created")
+    # message = {
+    #     "id": "erddsd",
+    #     "name": "project",
+    # }
+
+    # Test internet access
+    req = requests.get("https://dummyjson.com/products/1")
+    response = req.json()
     message = {
-        "id": "erddsd",
-        "name": "project",
+        "id": response["id"],
+        "name": response["title"]
     }
+
     sqs.send_message(
         QueueUrl = sqs_url,
         MessageBody = json.dumps(message)
