ca5086ac627dd4bac048571f65fe887983d42fcf
alixpeigue <irobot.moi@gmail.com>
2023-08-23 13:29:06 +0200

    Added cloudwatch rule for emitter lambda

--- emitter_lambda.tf
+++ emitter_lambda.tf
@@ -18,3 +18,23 @@ module "emitter_lambda" {
     sqs = aws_sqs_queue.queue.url
   }
 }
+
+/// EvenBridge Event
+
+resource "aws_cloudwatch_event_rule" "workday_replication_lambda_event_rule" {
+  name                = "daily-trigger-for-workday-replication"
+  schedule_expression = "rate(24 hours)"
+}
+
+resource "aws_cloudwatch_event_target" "workday_replication_lambda_target" {
+  arn  = module.emitter_lambda.function_arn
+  rule = aws_cloudwatch_event_rule.workday_replication_lambda_event_rule.name
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_invoke_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.emitter_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.workday_replication_lambda_event_rule.arn
+}
--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -2,6 +2,10 @@ output "function_name" {
   value = var.function_name
 }
 
+output "function_arn" {
+  value = aws_lambda_function.this.arn
+}
+
 output "execution_role_arn" {
   value = var.role != null ? var.role : aws_iam_role.this[0].arn
 }
